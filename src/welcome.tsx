841f29c3b3a3810b4ecf4c167fbfc10578d59feb
Suseendiran P <32263301+suseendiran@users.noreply.github.com>
2022-10-01 07:31:40 +0000

    Data type added for props

--- src/welcome.tsx
+++ src/welcome.tsx
@@ -1,7 +1,11 @@
-export const Welcome = ({imageUrl})=>{
+interface PropsType {
+    imageUrl: string
+}
+
+export const Welcome: React.FC<PropsType> = ({imageUrl})=>{
     return (
         <>
-        <h3>WELCOME INDIA</h4>
+        <h3>WELCOME INDIA</h3>
          <img
             src={imageUrl || '../assets/template-images/courses/vidocto_image.png'}
             alt=""

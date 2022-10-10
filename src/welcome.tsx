48d45bcd009feba555d7e3ce3626f52ad8413ea5
sarathiwinchart <115392951+sarathiwinchart@users.noreply.github.com>
2022-10-10 10:34:20 +0530

    Text file added

--- src/welcome.tsx
+++ src/welcome.tsx
@@ -2,14 +2,15 @@ interface PropsType {
     imageUrl: string
 }
 
-export const Welcome: React.FC<PropsType> = ({imageUrl})=>{
+export const Welcome: React.FC<PropsType> = ({ imageUrl }) => {
     return (
         <>
-        <h3>WELCOME INDIA</h3>
-         <img
-            src={imageUrl || '../assets/template-images/courses/vidocto_image.png'}
-            alt=""
-            className="img-fluid image-cover"
+            <h3>WELCOME INDIA</h3>
+            <p>Paragraphs are medium-sized units of writing, longer than sentences, but shorter than sections, chapters, or entire works. Because they connect the “small” ideas of individual sentences to a “bigger” idea, paragraph structure is essential to any writing for organization, flow, and comprehension. </p>
+            <img
+                src={imageUrl || '../assets/template-images/courses/vidocto_image.png'}
+                alt=""
+                className="img-fluid image-cover"
             />
         </>
     )
